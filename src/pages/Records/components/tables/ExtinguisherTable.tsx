0b995e4e3445c9feba65c86291c1864abc21bc16
Marcelo Silva <marcelorsdd@gmail.com>
2023-08-16 11:56:46 -0300

    feat(extinguisher): Add function to handleRowModal

--- src/pages/Records/components/tables/ExtinguisherTable.tsx
+++ src/pages/Records/components/tables/ExtinguisherTable.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { ptBR } from 'date-fns/locale';
 import { format, parseISO } from 'date-fns';
+import { useNavigate } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroller';
 
 import { Table } from '../../../../components/Table';
-import useExtinguisher from '../../hooks/useExtinguisher';
+import ExtinguisherModal from '../modals/ExtinguisherModal';
+import useExtinguisher, { Extinguisher } from '../../hooks/useExtinguisher';
 
 const ExtinguisherTable = () => {
   const { extinguisher, fetchNextPage, hasNextPage } = useExtinguisher();
+
+  const navigate = useNavigate();
+
+  const handleRowModal = (id: number) => {
+    navigate(`/records/${id}`);
+  };
 
   return (
     <>
@@ -28,7 +36,7 @@ const ExtinguisherTable = () => {
         </Table.Thead>
       </Table.Root>
 
-      <div className="min-[1100px]:h-[35.6rem] min-[1600px]:h-[41rem] min-[1800px]:h-[46rem]  w-full overflow-y-scroll">
+      <div className="min-[1100px]:h-[35.6rem] min-[1600px]:h-[41rem] min-[1800px]:h-[46rem] w-full overflow-y-scroll">
         <InfiniteScroll
           pageStart={0}
           loadMore={() => fetchNextPage()}
@@ -40,8 +48,12 @@ const ExtinguisherTable = () => {
             <Table.Tbody>
               {extinguisher?.pages.map((item, index) => (
                 <React.Fragment key={index}>
-                  {item?.data?.value?.map((item: any) => (
-                    <Table.Tr key={item.Id} className="hover:bg-[#E9F0F3] hover:cursor-pointer duration-200">
+                  {item?.data?.value?.map((item: Extinguisher) => (
+                    <Table.Tr
+                      onClick={() => handleRowModal(item.Id)}
+                      key={item.Id}
+                      className="hover:bg-[#E9F0F3] hover:cursor-pointer duration-200"
+                    >
                       <Table.Td className="pl-8">{item.Responsavel1.Title}</Table.Td>
                       <Table.Td>{format(parseISO(item.Created), 'dd MMM yyyy', { locale: ptBR })}</Table.Td>
                       <Table.Td>
@@ -64,6 +76,10 @@ const ExtinguisherTable = () => {
           </Table.Root>
         </InfiniteScroll>
       </div>
+
+      {/* {extinguisherItem && (
+        )} */}
+      <ExtinguisherModal />
     </>
   );
 };
