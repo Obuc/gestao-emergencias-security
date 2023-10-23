bfddab4b3c509d7d103bf9779daf20b7c6cbaeca
Marcelo Silva <marcelorsdd@gmail.com>
2023-10-23 15:03:51 -0300

    feat: Create EqHydrantsQRCodePdf

--- src/pages/Equipments/components/modals/EqGenerateQRCodeModal.tsx
+++ src/pages/Equipments/components/modals/EqGenerateQRCodeModal.tsx
@@ -1,10 +1,4 @@
-import { saveAs } from 'file-saver';
-import html2canvas from 'html2canvas';
-import { faExpand } from '@fortawesome/free-solid-svg-icons';
-
 import Modal from '../../../../components/Modal';
-import { Button } from '../../../../components/Button';
-import { ButtonIcon } from '../../../../components/Button/ButtonIcon';
 
 interface IEqGenerateQRCodeModalProps {
   open: boolean | null;
@@ -14,28 +8,6 @@ interface IEqGenerateQRCodeModalProps {
 
 const EqGenerateQRCodeModal = ({ open, onOpenChange, children }: IEqGenerateQRCodeModalProps) => {
   const equipments_value = localStorage.getItem('equipments_value');
-  const site_value = localStorage.getItem('user_site');
-
-  const generateQrCodePdf = () => {
-    const element = document.getElementById('qrCodeElement');
-
-    if (element) {
-      html2canvas(element, {
-        useCORS: true,
-        scale: 10,
-      })
-        .then((canvas) => {
-          canvas.toBlob((blob) => {
-            if (blob) {
-              saveAs(blob, `QRCode - ${site_value}.jpeg`);
-            }
-          }, 'image/jpeg');
-        })
-        .catch((error) => {
-          console.error('Erro ao gerar o PDF:', error);
-        });
-    }
-  };
 
   const handleOnOpenChange = () => {
     onOpenChange();
@@ -52,17 +24,6 @@ const EqGenerateQRCodeModal = ({ open, onOpenChange, children }: IEqGenerateQRCo
         <span className="text-lg py-4">Selecione abaixo os equipamentos que deseja gerar os QRCodes.</span>
 
         {children}
-
-        <div className="flex w-full gap-2 pt-14 justify-end items-center">
-          <Button.Root className="w-[10rem] h-10" onClick={onOpenChange}>
-            <Button.Label>Cancelar</Button.Label>
-          </Button.Root>
-
-          <Button.Root className="w-[14.0625rem] h-10" fill onClick={generateQrCodePdf}>
-            <Button.Label>Gerar QRCodes</Button.Label>
-            <ButtonIcon icon={faExpand} />
-          </Button.Root>
-        </div>
       </div>
     </Modal>
   );
---
+++ src/pages/Equipments/components/pdf/EqHydrantsQRCodePdf.tsx
@@ -0,0 +1,97 @@
+import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+
+import BXOLogo from '../../../../assets/BXOLogo.png';
+import HeaderBg from '../../../../assets/Caminho 3692.png';
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#FFF',
+    padding: 4,
+    flexWrap: 'wrap',
+    gap: 10,
+  },
+
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '6cm',
+    height: '8.5cm',
+    border: 1,
+    gap: 4,
+    borderColor: '#000',
+  },
+  header: {
+    height: 40,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  headerTitle: {
+    fontSize: 10,
+    textTransform: 'uppercase',
+    fontWeight: 600,
+    color: '#FFF',
+  },
+  headerBg: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: -1,
+  },
+  containerQrCode: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    gap: 4,
+  },
+  footerTitle: {
+    fontSize: 10,
+  },
+  containerSiteLogo: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  siteLogo: {
+    width: 30,
+    height: 32,
+  },
+});
+
+interface IEqHydrantsQRCodePdfProps {
+  data: Array<any>;
+}
+
+export const EqHydrantsQRCodePdf = ({ data }: IEqHydrantsQRCodePdfProps) => {
+  const generateQRCodeURL = (value: any) => {
+    const qrCodeValue = `Hidrantes;${value?.site};${value?.cod_qrcode}`;
+    return `https://chart.googleapis.com/chart?chs=95x75&cht=qr&chl=${qrCodeValue}`;
+  };
+
+  return (
+    <Document>
+      <Page size="A0" wrap style={styles.page}>
+        {data.map((value) => (
+          <View style={styles.container} key={value.Id} wrap={false}>
+            <View style={styles.header}>
+              <Text style={styles.headerTitle}>Gestão de Emergência</Text>
+              <Image src={HeaderBg} style={styles.headerBg} />
+            </View>
+
+            <View style={styles.containerQrCode}>
+              <Image src={generateQRCodeURL(value)} />
+              <Text style={styles.footerTitle}>{`Hidrante/${value?.site}/${value?.predio}/${value?.pavimento}`}</Text>
+              <Image src={BXOLogo} style={styles.siteLogo} />
+            </View>
+          </View>
+        ))}
+      </Page>
+    </Document>
+  );
+};
